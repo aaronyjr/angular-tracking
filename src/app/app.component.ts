aec975830f46b0089e135aa1ca8a6ac3642057ef
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-12-12 01:20:18 +0800

    Create routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MapComponent } from "./map/map.component";
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink],
 })
 export class AppComponent {
   title = 'angular-tracking';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MapComponent } from './map/map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: `Dashboard`, component: DashboardComponent },
+  { path: 'map', title: `Map`, component: MapComponent },
+];
