a63f588a92fd195dbaff105d1d0ea305cd00ce35
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-12-27 03:59:30 +0800

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   imports: [RouterOutlet, RouterLink],
 })
 export class AppComponent {
